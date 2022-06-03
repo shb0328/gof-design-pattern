37872b41a9dfa8411d68fbe27c580d30e713edd6
hyebb <hyebb@ncsoft.com>
2022-06-03 23:32:19 +0900

    싱글톤 패턴 2부 - 멀티 쓰레드 환경에서 안전한 방법3 (double checked locking)

--- src/main/java/gof/designpattern/singleton/Settings.java
+++ src/main/java/gof/designpattern/singleton/Settings.java
@@ -1,12 +1,19 @@
 package gof.designpattern.singleton;
 
 public class Settings {
-    private static final Settings INSTANCE = new Settings();
+    private static volatile Settings instance;
 
     public Settings() {
     }
 
     public static Settings getInstance() {
-        return INSTANCE;
+        if(instance == null) {
+            synchronized (Settings.class) {
+                if(instance == null){
+                    instance = new Settings();
+                }
+            }
+        }
+        return instance;
     }
 }
