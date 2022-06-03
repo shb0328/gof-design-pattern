33e6c08080eaf3b5ff18f69dda8b254258a90523
hyebb <hyebb@ncsoft.com>
2022-06-03 23:34:57 +0900

    싱글톤 패턴 2부 - 멀티 쓰레드 환경에서 안전한 방법4 (static inner class) (권장/lazy loading)

--- src/main/java/gof/designpattern/singleton/Settings.java
+++ src/main/java/gof/designpattern/singleton/Settings.java
@@ -1,19 +1,14 @@
 package gof.designpattern.singleton;
 
 public class Settings {
-    private static volatile Settings instance;
-
     public Settings() {
     }
 
+    private static  class SettingsHolder {
+        private static final Settings INSTANCE = new Settings();
+    }
+
     public static Settings getInstance() {
-        if(instance == null) {
-            synchronized (Settings.class) {
-                if(instance == null){
-                    instance = new Settings();
-                }
-            }
-        }
-        return instance;
+        return SettingsHolder.INSTANCE;
     }
 }
