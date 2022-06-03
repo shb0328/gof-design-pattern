c1c7bd9bdb1d0e1c9092844a09f451e313e1621a
hyebb <hyebb@ncsoft.com>
2022-06-04 00:02:36 +0900

    싱글톤 패턴 5부 - 자바와 스프링에서 찾아보는 패턴2 (Spring Bean)

---
+++ src/main/java/gof/designpattern/singleton/SpringConfig.java
@@ -0,0 +1,12 @@
+package gof.designpattern.singleton;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+    @Bean
+    public String hello() {
+        return "hello";
+    }
+}
