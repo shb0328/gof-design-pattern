efa70e8f05b8859fc4f466eb2ff4c4080d24f3ad
hyebb <hyebb@ncsoft.com>
2022-06-03 23:43:47 +0900

    싱글톤 패턴 3부 - 싱글톤 패턴을 깨트리는 방법2 (serialization & deserialization)

--- src/main/java/gof/designpattern/singleton/App.java
+++ src/main/java/gof/designpattern/singleton/App.java
@@ -1,14 +1,19 @@
 package gof.designpattern.singleton;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
+import java.io.*;
 
 public class App {
-    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         Settings settings1 = Settings.getInstance();
+        Settings settings2 = null;
 
-        Constructor<Settings> constructor = Settings.class.getDeclaredConstructor();
-        Settings settings2 = constructor.newInstance();
+        try(ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("settings.obj"))) {
+            out.writeObject(settings1);
+        }
+
+        try(ObjectInputStream in = new ObjectInputStream(new FileInputStream("settings.obj"))) {
+            settings2 = (Settings) in.readObject();
+        }
 
         System.out.println(settings1 == settings2);
     }
--- src/main/java/gof/designpattern/singleton/Settings.java
+++ src/main/java/gof/designpattern/singleton/Settings.java
@@ -1,6 +1,8 @@
 package gof.designpattern.singleton;
 
-public class Settings {
+import java.io.Serializable;
+
+public class Settings implements Serializable {
     private Settings() {
     }
 
