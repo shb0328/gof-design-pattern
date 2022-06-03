e2ea0eb6ec8e7f21ba50b81e7c32d1217d3315c8
hyebb <hyebb@ncsoft.com>
2022-06-03 23:14:10 +0900

    init springboot project

---
+++ src/main/java/gof/designpattern/GofDesignPatternApplication.java
@@ -0,0 +1,13 @@
+package gof.designpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GofDesignPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GofDesignPatternApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gof/designpattern/GofDesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package gof.designpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GofDesignPatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
