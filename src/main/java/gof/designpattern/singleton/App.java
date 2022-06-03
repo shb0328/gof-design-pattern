8402940e65c1e2865d74dd2bef526d158ea8cd1a
hyebb <hyebb@ncsoft.com>
2022-06-03 23:38:09 +0900

    싱글톤 패턴 3부 - 싱글톤 패턴을 깨트리는 방법1 (reflection)

--- src/main/java/gof/designpattern/singleton/App.java
+++ src/main/java/gof/designpattern/singleton/App.java
@@ -1,9 +1,15 @@
 package gof.designpattern.singleton;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
         Settings settings1 = Settings.getInstance();
-        Settings settings2 = Settings.getInstance();
+
+        Constructor<Settings> constructor = Settings.class.getDeclaredConstructor();
+        Settings settings2 = constructor.newInstance();
+
         System.out.println(settings1 == settings2);
     }
 }
