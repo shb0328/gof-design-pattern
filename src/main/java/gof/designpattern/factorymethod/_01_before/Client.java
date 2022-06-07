949bab83ffa20dc6506ca24d2bb4e9b51d3a793a
hyebeen <shb0328@gmail.com>
2022-06-07 22:23:48 +0900

    팩토리 메소드 패턴 1부 - 패턴 소개

---
+++ src/main/java/gof/designpattern/factorymethod/_01_before/Client.java
@@ -0,0 +1,13 @@
+package gof.designpattern.factorymethod._01_before;
+
+public class Client {
+
+    public static void main(String[] args) {
+        Ship whiteship = ShipFactory.orderShip("Whiteship", "keesun@mail.com");
+        System.out.println(whiteship);
+
+        Ship blackship = ShipFactory.orderShip("Blackship", "keesun@mail.com");
+        System.out.println(blackship);
+    }
+
+}
